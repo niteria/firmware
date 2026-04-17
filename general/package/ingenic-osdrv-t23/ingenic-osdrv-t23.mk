f863c4a26418bccbfd4a74c48719cb15658f2165
Bartosz Nitka <niteria@gmail.com>
2026-04-17 22:22:52 +0200

    ingenic-t23: add stock SC3336 sensor artifacts

--- general/package/ingenic-osdrv-t23/ingenic-osdrv-t23.mk
+++ general/package/ingenic-osdrv-t23/ingenic-osdrv-t23.mk
@@ -14,6 +14,8 @@ define INGENIC_OSDRV_T23_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensor
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T23_PKGDIR)/files/sensor/*.yaml
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T23_PKGDIR)/files/sensor/params/*.bin
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.10.14/ingenic
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.14/ingenic $(INGENIC_OSDRV_T23_PKGDIR)/files/kmod/*.ko
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(INGENIC_OSDRV_T23_PKGDIR)/files/script/*
@@ -26,6 +28,10 @@ define INGENIC_OSDRV_T23_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensor
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T23_PKGDIR)/files/sensor/$(OPENIPC_SNS_MODEL).yaml
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T23_PKGDIR)/files/sensor/params/$(OPENIPC_SNS_MODEL)-$(OPENIPC_SOC_MODEL).bin
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.10.14/ingenic
+	if [ -f $(INGENIC_OSDRV_T23_PKGDIR)/files/kmod/sensor_$(OPENIPC_SNS_MODEL)_$(OPENIPC_SOC_MODEL).ko ]; then \
+		$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.14/ingenic $(INGENIC_OSDRV_T23_PKGDIR)/files/kmod/sensor_$(OPENIPC_SNS_MODEL)_$(OPENIPC_SOC_MODEL).ko; \
+	fi
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(INGENIC_OSDRV_T23_PKGDIR)/files/script/*
